bdadf5560676604754c48eece4fab7c6cbabbf21
amin-mehrzad <43980359+amin-mehrzad@users.noreply.github.com>
2020-03-30 14:36:35 -0700

    chess board created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+var size = 8;
+var board = [1, 2, 3, 4];
 
 function App() {
+ 
+
+  function EvenOdd(index) {
+    console.log(index)
+    if ((index.index % 2)) {
+      return <div style={{
+        display: "flex",
+        flex: 1, flexDirection: "column"
+      }}>
+        <div style={{  backgroundColor:"black"   }} >
+          black
+    </div>
+        <div       style= {{backgroundColor:"white"}} >
+          white
+    </div>
+      </div>
+    } else {
+      return <div style={{
+        display: "flex",
+        flex: 1, flexDirection: "column"
+      }}>
+        <div       style= {{backgroundColor:"white"}} >
+          white
+  </div>
+  <div style={{  backgroundColor:"black"   }} >
+          black
+  </div>
+      </div>
+      //  } }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      {board.map((row, index) => (
+        <div key={index} style={{
+          display: "flex",
+          flex: 1, flexDirection: "row"
+        }}>
+          {board.map((column, index) => (
+            <EvenOdd index={index} />
+
+          ))}
+        </div>
+      ))
+  
+
+
+}
+
+    </div >
   );
 }
 
