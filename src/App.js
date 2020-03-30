22e62f5e4c7e561b8688a7fb00c028a8257e58c9
amin-mehrzad <43980359+amin-mehrzad@users.noreply.github.com>
2020-03-30 15:20:08 -0700

    unfinished

--- src/App.js
+++ src/App.js
@@ -1,62 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { IconContext } from "react-icons";
+import { FaRegCircle } from 'react-icons/fa';
+
 var size = 8;
 var board = [1, 2, 3, 4];
 
-function App() {
- 
-
-  function EvenOdd(index) {
-    console.log(index)
-    if ((index.index % 2)) {
-      return <div style={{
-        display: "flex",
-        flex: 1, flexDirection: "column"
-      }}>
-        <div style={{  backgroundColor:"black"   }} >
-          black
-    </div>
-        <div       style= {{backgroundColor:"white"}} >
-          white
-    </div>
-      </div>
-    } else {
-      return <div style={{
-        display: "flex",
-        flex: 1, flexDirection: "column"
-      }}>
-        <div       style= {{backgroundColor:"white"}} >
-          white
-  </div>
-  <div style={{  backgroundColor:"black"   }} >
-          black
-  </div>
-      </div>
-      //  } }
-    }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: "red" };
   }
-  return (
-    <div className="App">
+  render() {
 
-      {board.map((row, index) => (
-        <div key={index} style={{
+    function EvenOdd(index, rowIndex) {
+      console.log(index)
+      if ((index.index % 2)) {
+        return <div style={{
           display: "flex",
-          flex: 1, flexDirection: "row"
+          flex: 1, flexDirection: "column"
         }}>
-          {board.map((column, index) => (
-            <EvenOdd index={index} />
+          <div style={{ backgroundColor: "black" }} >
 
-          ))}
+            <IconContext.Provider value={{ color: "red", className: "global-class-name" }}>
+              <div>
+                <FaRegCircle />
+              </div>
+            </IconContext.Provider>
+          </div>
+          <div style={{ backgroundColor: "white" }} >
+            <IconContext.Provider value={{ color: "red", className: "global-class-name" }}>
+              <div>
+                <FaRegCircle />
+              </div>
+            </IconContext.Provider>
+          </div>
         </div>
-      ))
-  
+      } else {
+        return <div style={{
+          display: "flex",
+          flex: 1, flexDirection: "column"
+        }}>
+          <div style={{ backgroundColor: "white" }} >
+            white
+  </div>
+          <div style={{ backgroundColor: "black" }} >
+            black
+  </div>
+        </div>
+        //  } }
+      }
+    }
+    return (
+      <div className="App">
+
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} style={{
+            display: "flex",
+            flex: 1, flexDirection: "row"
+          }}>
+            {board.map((column, index) => (
+              <EvenOdd index={index} rowIndex={rowIndex} />
+
+            ))}
+          </div>
+        ))
 
 
-}
 
-    </div >
-  );
+        }
+
+      </div >
+    );
+  }
 }
 
 export default App;
